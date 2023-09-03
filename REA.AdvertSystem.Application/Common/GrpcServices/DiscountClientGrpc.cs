5adcee47cb86bd9393bae374f6be2703c5b37682
S8ight <fs.v1835@gmail.com>
2023-09-03 14:25:59 +0300

    A new service has been created for requests through gRPC to the service with discounts.

---
+++ REA.AdvertSystem.Application/Common/GrpcServices/DiscountClientGrpc.cs
@@ -0,0 +1,29 @@
+﻿
+using DiscountGrpcService.Protos;
+
+namespace REA.AdvertSystem.Application.Common.GrpcServices;
+
+public class DiscountClientGrpc
+{
+    private readonly DiscountProtoService.DiscountProtoServiceClient
+        _discountProtoClient;
+
+    public DiscountClientGrpc(DiscountProtoService.DiscountProtoServiceClient discountProtoClient)
+    {
+        _discountProtoClient = discountProtoClient;
+    }
+
+    public async Task<DiscountResponse> GetDiscount(string advertId, float price)
+    {
+        var request = new GetDiscountRequest { AdvertId = advertId, Price = price};
+        
+        return await _discountProtoClient.GetDiscountAsync(request);
+    }
+
+    public async Task DeleteAdvertDiscounts(string advertId)
+    {
+        var request = new DeleteDiscountRequest { AdvertId = advertId };
+        
+        await _discountProtoClient.DeleteAdvertDiscountsAsync(request);
+    }
+}
